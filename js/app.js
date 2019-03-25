2190387fe46e17deba9e5c99af7fee6c8b4367e0
Peter Mujuni <kontakt@petermujuni.dk>
2019-03-25 11:57:57 +0100

    add nick names to cat

--- js/app.js
+++ js/app.js
@@ -3,7 +3,8 @@ const ViewModel = function() {
     this.name = ko.observable('Tabby');
     this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
     this.imgAttribution = ko.observable('https://exampleUrl.dk');
-    this.catLevel = ko.observable('');
+    this.catLevel = ko.observable('Cat Level');
+    this.nickNames = ko.observableArray(['Nick','Topgun','King','Tiger']);
 
     this.incrementCounter = function() {
         this.clickCount(this.clickCount() +1);

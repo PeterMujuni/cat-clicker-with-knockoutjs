f1f5d3df61281af867b99edd6652037e0938ef8a
Peter Mujuni <kontakt@petermujuni.dk>
2019-03-25 14:04:22 +0100

    use with

--- js/app.js
+++ js/app.js
@@ -1,24 +1,33 @@
-const ViewModel = function() {
+const Cat = function() {
     this.clickCount = ko.observable(0);
     this.name = ko.observable('Tabby');
     this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
     this.imgAttribution = ko.observable('https://exampleUrl.dk');
-    this.catLevel = ko.observable('Cat Level');
     this.nickNames = ko.observableArray(['Nick','Topgun','King','Tiger']);
+    this.level = ko.computed(function() {
+        let level;
+        let clicks = this.clickCount();
+
+        if (clicks < 1) {
+            level = 'Newborn';
+        }else if (clicks < 5) {
+            level = 'Infant';
+        }else if (clicks < 10) {
+            level = 'Teen';
+        }else if (clicks > 9) {
+            level = 'Adult';
+        }
+        
+        return level;
+    }, this);
+};
+
+const ViewModel = function() {
+    let self = this;
+    this.currentCat = ko.observable(new Cat());
 
     this.incrementCounter = function() {
-        this.clickCount(this.clickCount() +1);
-        this.implementLevels();
-    };
-
-    this.implementLevels = function() {
-        if (this.clickCount() < 5) {
-            this.catLevel('Infant');
-        }else if (this.clickCount() < 10) {
-            this.catLevel('Teen');
-        }else if (this.clickCount() > 10) {
-            this.catLevel('Adult');
-        }
+        self.currentCat().clickCount(self.currentCat().clickCount() +1);
     };
 }
 
